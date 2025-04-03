172b404c47c69074400cb4b350036e6f3fbdee4d
LJM12914 <LJM12914@outlook.com>
2025-04-03 22:08:50 +0800

    migrate

--- old/scripts/archive-name.ps1
+++
@@ -1,6 +0,0 @@
-
-$archiveName = "v8_$Env:RUNNER_OS`_$($Env:RUNNER_ARCH.ToLower())"
-
-Write-Host "Using Archive Name: $archiveName"
-
-Write-Output "ARCHIVE_NAME=$archiveName" | Out-File -FilePath "$Env:GITHUB_ENV" -Encoding utf8
